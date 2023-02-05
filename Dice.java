9fb4bef7a6a0f450baed6eeee3a1a334d6e8d95a
TKCoder13 <TKCoding13@gmail.com>
2023-02-04 18:29:22 -0800

    Updated OneDie()

--- Dice.java
+++ Dice.java
@@ -8,16 +8,42 @@ public class Dice {
         return rn.nextInt(sides) + 1; // [1...6]
     }
     public static void OneDie() {
-        int[] rolls = {0, 0, 0, 0, 0};
-        int sidesOfDice = 5;
+        int[] rolls = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int sidesOfDice = 8;
         int N = 1000000;
         double sum = 0;
+        int currentRoll = 0;
         for (int i = 0; i < N; i++) {
-            ++rolls[roll(sidesOfDice) - 1];
+            currentRoll = roll(sidesOfDice);
+            if (currentRoll == 1) {
+                ++rolls[currentRoll];
+                // System.out.println(currentRoll + " @ " + "1");
+            } else if (currentRoll == 2) {
+                ++rolls[currentRoll];
+                // System.out.println(currentRoll + " @ " + "2");
+            } else if (currentRoll == 3) {
+                ++rolls[currentRoll];
+                // System.out.println(currentRoll + " @ " + "3");
+            } else if (currentRoll == 4) {
+                ++rolls[3];
+                // System.out.println(currentRoll + " @ " + "4");
+            } else if (currentRoll == 5) {
+                ++rolls[4];
+                // System.out.println(currentRoll + " @ " + "5");
+            } else if (currentRoll == 6) {
+                ++rolls[5];
+                // System.out.println(currentRoll + " @ " + "6");
+            } else if (currentRoll == 7) {
+                ++rolls[5];
+                // System.out.println(currentRoll + " @ " + "7");
+            } else {
+                ++rolls[5];
+                // System.out.println(currentRoll + " @ " + "8");
+            }
         }
         for (int i = 1; i <= sidesOfDice; i++) {  
-            System.out.println("The dice has rolled \"" + i + "\" " + rolls[i-1] + " times.");
-            System.out.println(i + " --> " + (double)rolls[i-1] / N);
+            System.out.println("The dice has rolled \"" + i + "\" " + rolls[i] + " times.");
+            System.out.println(i + " --> " + (double)rolls[i] / N);
             sum += (double)rolls[i-1] / N;
         }
         System.out.println("The sum of the probabilities is: " + sum);
@@ -43,6 +69,6 @@ public class Dice {
     }
 
     public static void main(String args[]) {
-        TwoDice(); // 0 and 1 should have no rolls
+        OneDie(); // 0 and 1 should have no rolls
     }
 }
